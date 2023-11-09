b79e3edc84dabeb89a1853f7f66477df323364f4
Ricardo Ferreira <rnhc1000@hotmail.com>
2023-11-08 23:59:59 -0300

    feat(SpringBoot) initial project setup....

---
+++ src/main/java/br/dev/ferreiras/turingReview/TuringReviewApplication.java
@@ -0,0 +1,13 @@
+package br.dev.ferreiras.turingReview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuringReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuringReviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/ferreiras/turingReview/TuringReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.ferreiras.turingReview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuringReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
