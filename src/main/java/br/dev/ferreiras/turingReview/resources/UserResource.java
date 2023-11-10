6e0c88e7b8d4e626784cdebde575188ffc34a7d8
Ricardo Ferreira <rnhc1000@hotmail.com>
2023-11-10 17:24:05 -0300

    feat(SpringBoot) Update user -> UserService & UserResource

--- src/main/java/br/dev/ferreiras/turingReview/resources/UserResource.java
+++ src/main/java/br/dev/ferreiras/turingReview/resources/UserResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,10 +51,18 @@ public class UserResource {
 
 	}
 
-	@DeleteMapping(value = "/id")
+	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		userService.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user) {
+
+		user = userService.update(id, user);
+		return ResponseEntity.ok().body(user);
+
 	}
 
 }
