00777c8c105c0016306a7018d7e5e909a3c828c2
Ricardo Ferreira <rnhc1000@hotmail.com>
2023-11-10 14:29:13 -0300

    feat(Spring Boot) Implemented SubTotal Per Order Item & Total per Order

--- src/main/java/br/dev/ferreiras/turingReview/entities/Order.java
+++ src/main/java/br/dev/ferreiras/turingReview/entities/Order.java
@@ -108,6 +108,18 @@ public class Order implements Serializable {
 		this.payment = payment;
 	}
 
+	public Double getTotal() {
+
+		double sum = 0.0;
+
+		for (OrderItem orderItem : items) {
+			sum += orderItem.getSubTotal();
+		}
+
+		return sum;
+
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
