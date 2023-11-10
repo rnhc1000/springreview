7c420f7143c48593ff869b5a5fa29e65d895fd0b
Ricardo Ferreira <rnhc1000@hotmail.com>
2023-11-10 14:45:23 -0300

    feat(Spring Boot) Insert User via @PostMapping and return 201 Created if succesful and location in the headers to show the location of the object created

--- src/main/java/br/dev/ferreiras/turingReview/resources/UserResource.java
+++ src/main/java/br/dev/ferreiras/turingReview/resources/UserResource.java
@@ -1,13 +1,17 @@
 package br.dev.ferreiras.turingReview.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.dev.ferreiras.turingReview.entities.User;
 import br.dev.ferreiras.turingReview.services.UserService;
@@ -32,6 +36,16 @@ public class UserResource {
 
 		User user = userService.findById(id);
 		return ResponseEntity.ok().body(user);
+
+	}
+
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User user) {
+
+		user = userService.insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{id}").buildAndExpand(user.getId()).toUri();
+
+		return ResponseEntity.created(uri).body(user);
 
 	}
 
